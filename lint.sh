d7e78ccb76fc22f70aed344a3fd980407f1fc550
sebastian <sebastian@sommerfeld.io>
2022-06-26 15:21:45 +0200

    feat: added linter to repo

---
+++ lint.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# @file lint.sh
+# @brief Update und run linters.
+#
+# @description The script updates linter definitions from the ``infrastructure`` repo and runs linters.
+#
+# ==== Arguments
+#
+# The script does not accept any parameters.
+
+
+echo -e "$LOG_INFO Download latest linter definitions"
+linterDefinitions=(
+  '.directoryvalidator.json'
+  '.ls-lint.yml'
+  '.yamllint.yml'
+)
+for file in "${linterDefinitions[@]}"; do
+  rm "$file"
+  curl -sL "https://raw.githubusercontent.com/sebastian-sommerfeld-io/infrastructure/main/resources/common-assets/linters/$file" -o "$file"
+  git add "$file"
+done
+
+echo -e "$LOG_INFO ------------------------------------------------------------------------"
+
+echo -e "$LOG_INFO Run linter containers"
+
+echo -e "$LOG_INFO check mandatory files"
+docker run -it --rm --volume "$(pwd):$(pwd)" --workdir "$(pwd)" sommerfeldio/directory-validator:latest directory-validator .
+
+echo -e "$LOG_INFO yamllint"
+docker run -it --rm --volume "$(pwd):/data" --workdir "/data" cytopia/yamllint:latest .
+
+echo -e "$LOG_INFO jsonlint"
+docker run -it --rm --volume "$(pwd):/data" --workdir "/data" cytopia/jsonlint:latest -i '*node_modules*' "*.json"
+
+echo -e "$LOG_INFO shellcheck"
+find . -not \( -path "*node_modules*" \) -name "*.sh" -exec docker run -it --rm --volume "$(pwd):/data" --workdir "/data" koalaman/shellcheck:latest {} \;
+
+echo -e "$LOG_INFO lint Dockerfile"
+find . -not \( -path "*node_modules*" \) -name Dockerfile -exec \
+    sh -c 'src=${1#./} && echo "$LOG_INFO Lint $1" && docker run -i  --rm hadolint/hadolint < $1' sh "{}" \;
+
+echo -e "$LOG_INFO lint Vagrantfile"
+echo -e "$LOG_WARN todo ..." # todo ...
+
+echo -e "$LOG_INFO lslint"
+docker run -it --rm --volume "$(pwd):/data" --workdir "/data" lslintorg/ls-lint:1.11.0
+
+echo -e "$LOG_INFO lint .env files"
+docker run -it --rm --volume "$(pwd):/app" --workdir "/app" dotenvlinter/dotenv-linter:latest --exclude "*node_modules*" --recursive
+
+echo -e "$LOG_INFO lint terraform files"
+find . -not \( -path "*node_modules*" \) -name "*.tf" -exec docker run -it --rm --volume "$(pwd):$(pwd)" --workdir "$(pwd)" ghcr.io/terraform-linters/tflint:latest {} \;
+
+echo -e "$LOG_INFO ------------------------------------------------------------------------"
+
+echo -e "$LOG_DONE Finished linting"
